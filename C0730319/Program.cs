4336a9e87e020bf6250930cdde99a5c32bec4968
visualstudio <visualstudio@122-PC17>
2019-01-18 21:59:55 -0500

    Final Code

--- C0730319/Program.cs
+++ C0730319/Program.cs
@@ -10,6 +10,43 @@ namespace C0730319
     {
         static void Main(string[] args)
         {
+            {
+                {
+                    StudentInformation _Mary = new StudentInformation("Mary", 3.8, 20);
+                    StudentInformation _john = new StudentInformation("john", 3.8, 20);
+                    StudentInformation _mark = new StudentInformation("mark", 3.8, 20);
+                    StudentInformation _steve = new StudentInformation("steve", 3.8, 20);
+                    StudentInformation _cindy = new StudentInformation("cindy", 3.8, 20);
+                    var studentlist = new List<StudentInformation>();
+                    studentlist.Add(_Mary);
+                    studentlist.Add(_john);
+                    studentlist.Add(_mark);
+                    studentlist.Add(_steve);
+                    studentlist.Add(_cindy);
+                    foreach (var student in studentlist)
+                    {
+                        Console.WriteLine(student.Name);
+                    }
+                    Console.ReadLine();
+                }
+            }
+        }
+
+        class StudentInformation
+
+        {
+            public string Name;
+            public double GPA;
+            public int Attendence;
+            public StudentInformation(string aName, double cGPA, int dAttendence)
+
+            {
+                Name = aName;
+
+                GPA = cGPA;
+                Attendence = dAttendence;
+            }
         }
     }
 }
+
